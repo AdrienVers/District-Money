ab6051be00c7784141dd2c6d57e7ba713102b258
AdrienVers <adrienvx77@outlook.fr>
2022-11-20 18:42:42 +0100

    Addition of E2E tests

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,8 @@
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("https://district-money.vercel.app/");
+    cy.get('a[href*="login"]').click();
+    cy.url().should("include", "/login");
+    cy.get("h1").contains("Connexion");
+  });
+});
