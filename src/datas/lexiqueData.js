89a98aeafd3336b575d3b40d6fcbde24a8721297
AdrienVers <adrienvx77@outlook.fr>
2022-12-09 19:10:27 +0100

    Addition of a Lexique component

---
+++ src/datas/lexiqueData.js
@@ -0,0 +1,114 @@
+export const LEXIQUE_DATA = [
+  {
+    name: "Action",
+    definition: "Titre de propriété d'une part du capital d'une société.",
+    tag: "Produit",
+  },
+  {
+    name: "AMF (Autorité des marchés financiers)",
+    definition: "Organisme régulateur des marchés français.",
+    tag: "Organisme",
+  },
+  {
+    name: "Blockchain",
+    definition:
+      "La blockchain est une technologie de stockage et de transmission d'informations, transparente, sécurisée, et fonctionnant sans organe central de contrôle.",
+  },
+  {
+    name: "Cryptomonnaie",
+    definition:
+      "La cryptomonnaie est monnaie existant sous forme  virtuelle et utilisant la cryptographie pour sécuriser les transactions.",
+  },
+  {
+    name: "Dividende",
+    definition: "Part du bénéfice distribuée aux actionnaires.",
+    tag: "Bénéfice",
+  },
+  {
+    name: "DSP 2 (Directive sur les services de paiement)",
+    definition:
+      "Directive européenne mise en application le 14 septembre 2019, ayant pour objectifs de mieux informer et protéger les consommateurs face à la cybercriminalité, au vol de données et aux fraudes. et de garantir un accès équitable et ouvert aux marchés de paiement.",
+    tag: "Organisme",
+  },
+  {
+    name: "ETF (Trackers)",
+    definition: "reproduit les évolutions d'un indice boursier.",
+    tag: "Produit.",
+  },
+  {
+    name: "Indice",
+    definition: "",
+  },
+  {
+    name: "Moins-value",
+    definition:
+      "Perte enregistée au moment de la cession (vente) d'un actif, calculée en faisant la différence entre le prix d'achat et le prix de vente (y compris avec les frais de transaction).",
+    tag: "Bénéfice",
+  },
+  {
+    name: "Obigation",
+    definition:
+      "Titre de créance négociable représentant une part de la dette d'un État ou d'une entreprise.",
+    tag: "Produit",
+  },
+  {
+    name: "Obligation conversible (OC)",
+    definition:
+      "Obligation pouvant être échangée à tout moment contre une nouvelle action de la société émettrice.",
+    tag: "Produit",
+  },
+  {
+    name: "OPCVM",
+    definition: "",
+    tag: "Produit",
+  },
+  {
+    name: "Option",
+    definition: "",
+    tag: "Produit",
+  },
+  {
+    name: "PEA (Plan d'épargne en action)",
+    definition: "",
+    tag: "Compte",
+  },
+  {
+    name: "PER (Price Earning Ratio)",
+    definition: "Rapport entre le cours et le bénéfice d'une action.",
+  },
+  {
+    name: "Portefeuille",
+    definition: "",
+  },
+  {
+    name: "Plus-value",
+    definition:
+      "Gain enregisté au moment de la cession (vente) d'un actif, calculée en faisant la différence entre le prix d'achat et le prix de vente (y compris avec les frais de transaction).",
+  },
+  {
+    name: "SICAV",
+    definition: "",
+  },
+  {
+    name: "SRD",
+    definition: "",
+  },
+  {
+    name: "Turbo",
+    definition: "",
+  },
+  {
+    name: "Valeur immobilière",
+    definition: "",
+  },
+  {
+    name: "Valeur mobilière",
+    definition:
+      "Terme désignant tous les titres négociables cotés ou non en Bourse.",
+  },
+  {
+    name: "Warrant",
+    definition:
+      "Valeur mobilière émise par un établissement financier ayant le caractère d'une option.",
+  },
+];
